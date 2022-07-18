0ae38eb310264fe0d3c8f8aa30a1cfaac167bc24
Truong Ngoc Hai <haitnhe61@gmail.com>
2022-07-19 06:10:18 +0700

    Add wallet

---
+++ app/src/main/java/adapter/WalletViewAdapter.java
@@ -0,0 +1,94 @@
+package adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mywallet.R;
+import com.example.mywallet.WalletFragment;
+
+import java.util.List;
+
+import dao.AppDatabase;
+import entity.Transaction;
+import entity.Wallet;
+
+public class WalletViewAdapter extends BaseAdapter{
+
+private WalletFragment walletFragment;
+    private Context context;
+    private List<Wallet> listWallet;
+
+    public WalletViewAdapter(Context context, List<Wallet> listWallet) {
+        this.context = context;
+        this.listWallet = listWallet;
+    }
+
+    public WalletViewAdapter(WalletFragment walletFragment, List<Wallet> wallets) {
+        this.walletFragment = walletFragment;
+        this.listWallet = wallets;
+    }
+
+    @Override
+    public int getCount() {
+        return listWallet.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        if(position < 0){
+            return null;
+        }
+        return listWallet.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if(convertView == null){
+            convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_wallet, null);
+        }
+
+        ImageView imageViewWallet = convertView.findViewById(R.id.imageViewWallet);
+        TextView nameWallet = convertView.findViewById(R.id.nameWallet);
+        TextView balanceWallet = convertView.findViewById(R.id.balanceWallet);
+
+        Wallet wallet = listWallet.get(position);
+//        imageViewWallet.setImageResource(wallet.get);
+        nameWallet.setText(wallet.getName());
+        balanceWallet.setText(Integer.toString(wallet.getBalance()));
+
+        Button editWallet = convertView.findViewById(R.id.editWallet);
+        Button editWalletMenu = convertView.findViewById(R.id.editWalletMenu);
+        Button deleteWalletMenu = convertView.findViewById(R.id.deleteWalletMenu);
+
+        editWallet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popupMenu = new PopupMenu(parent.getContext(), editWallet);
+                popupMenu.inflate(R.menu.layout_popup_menu);
+                Menu menu = popupMenu.getMenu();
+                popupMenu.show();
+            }
+        });
+        return convertView;
+    }
+
+
+
+}
--- app/src/main/java/com/example/mywallet/WalletFragment.java
+++ app/src/main/java/com/example/mywallet/WalletFragment.java
@@ -1,18 +1,63 @@
 package com.example.mywallet;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.room.Room;
+
+import java.util.List;
+
+import adapter.WalletViewAdapter;
+import dao.AppDatabase;
+import dao.WalletDao;
+import entity.Wallet;
 
 public class WalletFragment extends Fragment {
+    private WalletViewAdapter adapter;
+//    private List<Wallet> listWallet;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_wallet,container,false);
     }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ListView listViewWallet = view.findViewById(R.id.listViewWallet);
+        AppDatabase db = Room.databaseBuilder(getActivity().getApplicationContext(),
+        AppDatabase.class, "mywallet").allowMainThreadQueries().build();
+
+        WalletDao walletDao = db.walletDao();
+        List<Wallet> wallets = walletDao.getAll();
+
+        adapter = new WalletViewAdapter(this, wallets);
+        listViewWallet.setAdapter(adapter);
+        int totalBalance=0;
+        TextView totalBalanceWallet = view.findViewById(R.id.totalBalanceWallet);
+        for(Wallet wallet:wallets)
+            totalBalance+=wallet.getBalance();
+        totalBalanceWallet.setText("Total Balance: "+ totalBalance+ "₫");
+        TextView addWallet = view.findViewById(R.id.addWallet);
+        addWallet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(this,AddWallet.class);
+            }
+        });
+    }
+
+
 }
